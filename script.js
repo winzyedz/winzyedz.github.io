21468678e46f037999daa245440b93f81943f674
evenedx85-cell <evenedx85@gmail.com>
2025-08-14 21:28:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+    console.log('สัส! มึงมาแล้วเหรอ? 😏 นี่คือ WNZY X Official Web!');
+    console.log('เว็ปนี้ responsive ทั้งมือถือและ PC ปุ่มไอค่อน + GIF 16:9 มีครบ');
+});
