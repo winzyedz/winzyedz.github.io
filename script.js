cd43cd9a20d34c443cbbf959427ca634fc7f1e07
winzyedz <evenedx85@gmail.com>
2026-04-21 14:41:51 +0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', () => {
-    console.log('สัส! มึงมาแล้วเหรอ? 😏 นี่คือ WNZY X Official Web!');
-    console.log('เว็ปนี้ responsive ทั้งมือถือและ PC ปุ่มไอค่อน + GIF 16:9 มีครบ');
-});
